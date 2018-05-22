8e83fb8dcefbf4821ce664e75b8a28b7a7ff0483
anderscodes <anders.j.persson@hotmail.com>
2018-05-22 15:44:30 +0100

    seeder for listings created

--- server/models/listing.js
+++ server/models/listing.js
@@ -1,11 +1,30 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Listing = sequelize.define('Listing', {
-    id: DataTypes.INTEGER,
-    title: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    price: DataTypes.INTEGER,
-    user_id: DataTypes.INTEGER
+    id: {
+      type: DataTypes.SERIAL,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    title: {
+      type: DataTypes.STRING,
+      notNull: true
+    }
+
+    description: {
+      type: DataTypes.TEXT,
+      notNull: true
+    },
+
+    price: {
+      type: DataTypes.INTEGER,
+      notNull: true
+    },
+
+    user_id: {
+      type: DataTypes.INTEGER,
+
   }, {});
   Listing.associate = function(models) {
     // associations can be defined here
---
+++ server/seeders/20180522143526-demo-listing.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+      return queryInterface.bulkInsert('Listings', [{
+        title: 'Simply stunning period apartment',
+        description: 'this property is incredile',
+        price: 4000
+      }], {});
+
+  },
+
+  down: (queryInterface, Sequelize) => {
+
+      return queryInterface.bulkDelete('Listings', null, {});
+
+  }
+};
